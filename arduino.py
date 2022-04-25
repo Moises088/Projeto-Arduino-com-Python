3e2d69d5e7ebbd0c7c42b870018eeb6d2715c2f0
Moises088 <costamoises300@gmail.com>
2022-04-24 22:59:40 -0300

    feat: definindo os pinos dos leds

--- arduino.py
+++ arduino.py
@@ -2,3 +2,9 @@ from pyfirmata import Arduino, util, SERVO
 from time import time, sleep
 
 board = Arduino('COM3')
+
+# Variavéis do semafóro
+dig_semaforo_vermelho = 4
+dig_semaforo_amarelo = 3
+dig_semaforo_verde = 2
+
