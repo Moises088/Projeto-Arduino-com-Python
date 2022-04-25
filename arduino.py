94a77331f8124d27e40b2b6f19418a58427e5e78
Moises088 <costamoises300@gmail.com>
2022-04-24 23:06:57 -0300

    feat: Adicionando funções de display e servo motor
    função de buscar o numero correspondente na matriz e ativar as portas corretas do display
    
    funções de rotacionar o servo motor 0 ~ 90, com o tempo variavel

--- arduino.py
+++ arduino.py
@@ -8,7 +8,7 @@ dig_semaforo_vermelho = 4
 dig_semaforo_amarelo = 3
 dig_semaforo_verde = 2
 
-# matriz do display
+# Portas e matriz correspondente ao display de 7 segmentos
 display_pinos = [10, 12, 7, 8, 9, 13, 11, 6]
 display_digitos = [
     [1,1,1,1,1,1,0,0],
@@ -23,9 +23,27 @@ display_digitos = [
     [1,1,1,0,0,1,1,0]
 ]
 
-# servo motor
+# Porta PWM do servo motor
 servo_pin = board.get_pin('d:5:p')
 servo_pin.mode = SERVO
+
+# Função para exibir o número no display
+def getDisplayNumber(board, display_pinos, display_digitos, number):
+    for i in range(8):
+        board.digital[display_pinos[i]].write(display_digitos[number][i])
+
+# Função para "abrir" o servo motor (rotacionar aproximadamente 90 graus)
+def rotateServoOpen(servo_pin):
+    for i in range(11):
+        servo_pin.write(9.2 * i)
+        sleep(0.5 / (i + 0.2))
+
+# Função para "fechar" o servo motor (rotacionar aproximadamente 0 graus)
+def rotateServoClose(servo_pin):
+    for i in range(10):
+        servo_pin.write(90 - (i * 10))
+        sleep(0.5 / (i + 0.2))
+
 
 
 
