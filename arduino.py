5a8d35be4febc7241e5b7e833e6b36c03873772c
Moises088 <costamoises300@gmail.com>
2022-04-24 23:01:58 -0300

    feat: criando variaveis do display e do servo
    criando matriz para ativar os números do display de 7 segmentos
    
    definindo o servo motor na porta pwm

--- arduino.py
+++ arduino.py
@@ -3,8 +3,30 @@ from time import time, sleep
 
 board = Arduino('COM3')
 
-# Variavéis do semafóro
+# Variavéis do semafóro (leds)
 dig_semaforo_vermelho = 4
 dig_semaforo_amarelo = 3
 dig_semaforo_verde = 2
+
+# matriz do display
+display_pinos = [10, 12, 7, 8, 9, 13, 11, 6]
+display_digitos = [
+    [1,1,1,1,1,1,0,0],
+    [0,1,1,0,0,0,0,0],
+    [1,1,0,1,1,0,1,0],
+    [1,1,1,1,0,0,1,0],
+    [0,1,1,0,0,1,1,0],
+    [1,0,1,1,0,1,1,0],
+    [1,0,1,1,1,1,1,0],
+    [1,1,1,0,0,0,0,0],
+    [1,1,1,1,1,1,1,0],
+    [1,1,1,0,0,1,1,0]
+]
+
+# servo motor
+servo_pin = board.get_pin('d:5:p')
+servo_pin.mode = SERVO
+
+
+
 
